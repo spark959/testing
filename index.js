b283445aa4bfa0355a1254e5211266c9da2af997
spark959 <babbott55@byu.edu>
2019-05-10 14:55:02 -0600

    This is just a test

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-//herro
+const request = require('request');
+
+request.get('http://www.google.com', function (error, response, body) {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body:', body);
+})
